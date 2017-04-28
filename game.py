28981dc800832556193de7537fe024597de39d5a
Merilee Wheelock <eelirem@Merilees-MacBook-Air.local>
2017-04-27 20:44:45 -0400

    Shooter game updates

--- game.py
+++ game.py
@@ -5,6 +5,10 @@ import pygame
 from Player import Player
 #Import check_events from the game_functions module
 from game_functions import check_events
+#Get the enemy class
+from Enemy import Enemy
+#Get groupcollide and Group from the sprite module
+from pygame.sprite import Group, groupcollide
 
 #The core game functionality/loop
 def run_game():
@@ -19,16 +23,43 @@ def run_game():
 	#Set a caption on the terminal window
 	pygame.display.set_caption("A Heroic 3rd Person Shooter")
 
-	the_player = Player(screen)
+	the_player = Player(screen, "./images/Hero.png", 100, 100)
+	bad_guy = Enemy(screen)
+	the_player_group = Group()
+	the_player_group.add(the_player) #"add" is like append since this is owned by pygame
+	enemies = Group()
+	enemies.add(bad_guy)
+	bullets = Group()
+
+	tick = 0
 
 	#Main game loop. Run forever... (or until break)
 	while 1: #use "while 1" if the code is fairly simple (game_on = True for more complex)
+		tick += 1
+		if tick % 50 == 0:
+			enemies.add(Enemy(screen))
 		screen.fill(background_color)
 
-		check_events()
+		check_events(the_player, screen, bullets)
 		
 		#Draw the player
-		the_player.draw_me()
+		for player in the_player_group: #player will only be drawn based on hero_died below
+			player.draw_me()
+
+		#Update and draw the bullets
+		for bullet in bullets:
+			bullet.update()
+			bullet.draw_bullet()
+
+		#Update and draw the bad guy
+		for bad_guy in enemies:
+			bad_guy.update_me(the_player)
+			bad_guy.draw_me()
+
+		#Check for collisions...
+		hero_died = groupcollide(the_player_group, enemies, True, False) #will make hero die when they collide
+		# print hero_died
+		groupcollide(bullets, enemies, True, True)
 
 		#Clear the screen for the next time through the loop
 		pygame.display.flip()
--- game_functions.py
+++ game_functions.py
@@ -1,10 +1,33 @@
 import pygame
 #Get the sys module so we can exit the program
 import sys
+from Bullet import Bullet
 
-def check_events():
+def check_events(the_player, screen, bullets):
 	#The escape hatch (from while)
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			#The user clicked the red X. Get out of the loop and kill the game.
 			sys.exit()
+		elif event.type == pygame.KEYDOWN:
+			if event.key == 273:
+				the_player.should_move("up", True)
+			elif event.key == 274:
+				the_player.should_move("down", True)
+			elif event.key == 276:
+				the_player.should_move("left", True)
+			elif event.key == 275:
+				the_player.should_move("right", True)
+			elif event.key == 32: #spacebar to FIRE
+				for direction in range(1, 5):
+					new_bullet  = Bullet(screen, the_player, direction)
+					bullets.add(new_bullet)
+		elif event.type == pygame.KEYUP:
+			if event.key == 273:
+				the_player.should_move("up", False)
+			elif event.key == 274:
+				the_player.should_move("down", False)
+			elif event.key == 276:
+				the_player.should_move("left", False)
+			elif event.key == 275:
+				the_player.should_move("right", False)
